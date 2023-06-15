360f2e33894b0a0a355f3722a7ade076de08c304
Prottasha19 <pghosh@uni-bremen.de>
2023-06-16 00:03:23 +0600

    merge two pdf file togather

---
+++ pdfmerger.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+
+import PyPDF2
+import sys
+import os
+
+merger = PyPDF2.PdfFileMerger()
+
+for file in os.listdir(os.curdir):
+	if file.endswith(".pdf"):
+		
+		merger.append(file)
+	merger.write("combined_PDF.pdf")
+		
+
+
+
+
